bba7a26c8258fd8bb8c1acd6663500063ffe0c79
johannes112 <johannes.emmos@gmail.com>
2020-06-08 22:46:58 +0200

    extensions: added functions to get first real value of a fetched rrd for better graphes

--- rrdtool_wrapper.py
+++ rrdtool_wrapper.py
@@ -9,6 +9,26 @@ import sys
 import rrdtool
 import os.path
 from os import path
+from datetime import datetime
+
+def convert_timestamp_to_date(timestamp):
+    date_timestamp_obj = datetime.fromtimestamp(timestamp)
+    return(date_timestamp_obj)
+
+#get the first timestamp which have a value of numbers
+#iterate over a already defined database and look for values which are not 'none'
+def look_for_real_values(fetch_data, start, fetch_data_heartbeat):
+    array_real_values = []
+    fetch_data_timestamp = int(start)
+    # print(f"fetch_data_timestamp{type(fetch_data_timestamp)}{fetch_data_timestamp}")
+    for fetch_data_entry in fetch_data:
+        fetch_data_content = str(fetch_data_entry[0])
+        if(fetch_data_content!='None'):
+            string_real_values = f"[{fetch_data_timestamp}][{fetch_data_content}]"
+            array_real_values.append(string_real_values)
+            # fetch_data_timestamp =+ int(fetch_data_heartbeat)
+        fetch_data_timestamp = fetch_data_timestamp + int(fetch_data_heartbeat)
+    return array_real_values
 
 def creator_rrd(rrdfile_name, starttime, step):
     create_status_msg = ""
@@ -17,17 +37,11 @@ def creator_rrd(rrdfile_name, starttime, step):
             rrdfile_name,
             "--start", starttime,
             "--step", step,
-            # "DS:etoday:GAUGE:600:0:50000",
-            # "RRA:AVERAGE:0.5:1:200",
-            # "RRA:AVERAGE:0.5:200:7",
-            # "RRA:AVERAGE:0.5:1400:4",
-            # "RRA:AVERAGE:0.5:5200:12",
-            # "RRA:AVERAGE:0.5:62400:10")
             "DS:etoday:GAUGE:600:0:50000",
-            "RRA:AVERAGE:0.5:1:600",
-            "RRA:AVERAGE:0.5:6:700",
-            "RRA:AVERAGE:0.5:24:775",
-            "RRA:AVERAGE:0.5:288:797",
+            "RRA:AVERAGE:0.5:1:600",  #5  min
+            "RRA:AVERAGE:0.5:6:700",  #30 min
+            "RRA:AVERAGE:0.5:24:775", #2  std
+            "RRA:AVERAGE:0.5:288:797",#1 day
             "RRA:MIN:0.5:1:600",
             "RRA:MIN:0.5:6:700",
             "RRA:MIN:0.5:24:775",
@@ -70,11 +84,11 @@ def updater_rrd(rrdfile_name, value):
 def info_rrd(rrdfile_name):
     info_content = []
     info_status_msg = ""
-    #is the given file a rrd
+    #get the last value
     try:
         db_info = rrdtool.info(rrdfile_name)
         info_content = db_info
-        # print(db_info)
+        #print(db_info)
         info_status_msg = f"success: {rrdfile_name}: was read successfully"
     except:
         info_status_msg = f"error: rrd update error: {sys.exc_info()[1]}"
@@ -83,27 +97,84 @@ def info_rrd(rrdfile_name):
     info_msg['status'] = info_status_msg
     return info_msg
 
-def fetch_rrd(rrdfile_name):
-    fetch_content = []
-    fetch_status_msg = ""
+def get_first_timestamp_rrd(rrdfile_name):
+    first_timestamp = []
+    get_first_status_msg = ""
     #is the given file a rrd
     try:
-        db_values = rrdtool.fetch(rrdfile_name, "AVERAGE")
-        # start, end, step = result[0]
-        # ds = result[1]
-        # rows = result[2]
-        fetch_times = db_values[0]
+        db_first_timestamp = rrdtool.first(rrdfile_name)
+        get_first_status_msg = f"success: first timestamp of {rrdfile_name} was found"
+    except:
+        get_first_status_msg = f"error: get_first_timestamp_rrd({rrdfile_name}) was not possible: {sys.exc_info()[1]}"
+    get_first_msg = dict()
+    get_first_msg['first_timestamp'] = db_first_timestamp
+    get_first_msg['status'] = get_first_status_msg
+    return get_first_msg
+
+def get_last_timestamp_rrd(rrdfile_name):
+    last_timestamp = []
+    get_last_status_msg = ""
+    try:
+        db_last_timestamp = rrdtool.last(rrdfile_name)
+        get_last_status_msg = f"success: last timestamp of {rrdfile_name} was found"
+    except:
+        get_last_status_msg = f"error: get_last_timestamp_rrd({rrdfile_name}) was not possible: {sys.exc_info()[1]}"
+    get_last_msg = dict()
+    get_last_msg['last_timestamp'] = db_last_timestamp
+    get_last_msg['status'] = get_last_status_msg
+    return get_last_msg
+
+
+def get_last_update_rrd(rrdfile_name):
+    last_value = []
+    get_last_update_status_msg = ""
+    try:
+        db_last_value = rrdtool.lastupdate(rrdfile_name)
+        get_last_update_status_msg = f"success: last value of {rrdfile_name} was found"
+    except:
+        get_last_update_status_msg = f"error: get_last_value_rrd({rrdfile_name}) was not possible: {sys.exc_info()[1]}"
+    get_last_value_msg = dict()
+    get_last_value_msg['last_value'] = db_last_value
+    get_last_value_msg['status'] = get_last_update_status_msg
+    return get_last_value_msg
+
+
+def fetch_rrd(rrdfile_name, cf, start, end):
+    fetch_content = []
+    fetch_status_msg = ""
+    fetch_data = ""
+    fetch_data_value = ""
+    fetch_data_value_first = ""
+    fetch_data_first_real_value = 0
+    try:
+        db_values = rrdtool.fetch(rrdfile_name,
+                                    cf,
+                                    '--start', start,
+                                    '--end', end
+                                    )
+        fetch_data_heartbeat = db_values[0][2]
+        fetch_data = db_values[2]
+        fetch_data_first_real_value = look_for_real_values(fetch_data, start, fetch_data_heartbeat)[0]
+        #print(f"fetch_data_first_real_value{type(fetch_data_first_real_value)}{fetch_data_first_real_value}")
         fetch_status_msg = f"success: {rrdfile_name}: was fetched successfully"
     except:
         fetch_status_msg = f"error: rrd fetch error: {sys.exc_info()[1]}"
         print("fetch error")
     fetch_msg = dict()
-    fetch_msg['times'] = fetch_times
+    fetch_msg['data'] = fetch_data
+    fetch_msg['first_real_value'] = fetch_data_first_real_value
     fetch_msg['status'] = fetch_status_msg
     return fetch_msg
 
 
 def grapher_rrd(rrd_filename, devicename, image_name, image_typ, starttime, endtime, last_time, last_update):
+    last_time_date_format_de = (last_time.split(' ')[0]).replace('/', '.')
+    last_time_time = last_time.split(' ')[1]
+    last_time_time_point = last_time_time.replace(':', '.')
+    starttime_date_string = str(convert_timestamp_to_date(int(starttime)))
+    starttime_date_point = starttime_date_string.replace(':', '.')
+    # print(f"grapher_rrd: starttime_date_point {type(starttime_date_point)}:{starttime_date_point}")
+    string_from_to = f"From {starttime_date_point} To {last_time_date_format_de} {last_time_time_point}"
     graph_status_msg = ""
     rrd_def = f"DEF:kwh_etoday={rrd_filename}:etoday:AVERAGE"
     try:
@@ -114,14 +185,15 @@ def grapher_rrd(rrd_filename, devicename, image_name, image_typ, starttime, endt
                       '--start', starttime,
                       '--end', endtime,
                       '--font', "DEFAULT:7",
-                      '--title', f"{devicename}: Generated Energy of {(last_time.split(' ')[0]).replace('/', '.')}: {last_update}kwh",
+                      '--title', f"{devicename}: Generated Energy of {last_time_date_format_de}: {last_update}kwh",
                       '--watermark', "date",
                       '--vertical-label', "kwh",
-                      '--right-axis-label', f"last update time: {last_time.split(' ')[1]}",
+                      '--right-axis-label', f"last update time: {last_time_time}",
                       f"{rrd_def}",
-                      "LINE2:kwh_etoday#FF0000",
-                      'COMMENT:"From 2020/05/25 20\:17\:07 To 2020/05/26 20\:12\:07\c"')
+                      "LINE2:kwh_etoday#FF0000"
+                      ,f"COMMENT: {string_from_to}"
+                      )
         graph_status_msg = f"success: {image_name} was successfully generated"
     except:
-        graph_status_msg = f"error: rrd graph error: {sys.exc_info()[1]}"
+        graph_status_msg = f"error: rrd graph error: {sys.exc_info()[1]} \nargs: {type(rrd_filename)}:{rrd_filename}, {type(devicename)}:{devicename}, {type(image_name)}:{image_name}, {type(image_typ)}:{image_typ}, {type(starttime)}:{starttime}, {type(endtime)}:{endtime}, {type(last_time)}:{last_time}, {type(last_update)}:{last_update}"
     return graph_status_msg
